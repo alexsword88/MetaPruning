c6e974f9d328acfca55d4b61e74fcd22f81f3633
alexsword88 <alexsword88@gmail.com>
2022-05-17 21:41:14 +0800

    feat: make ouput not buffer

--- mobilenetv1/evaluating/run.sh
+++ mobilenetv1/evaluating/run.sh
@@ -1,2 +1,2 @@
 clear
-python3 evaluate.py --epochs=64 --batch_size=128 --workers=2 | tee -a log/training.txt
+python3 -u evaluate.py --epochs=64 --batch_size=128 --workers=2 | tee -a log/training.txt
--- mobilenetv1/searching/run.sh
+++ mobilenetv1/searching/run.sh
@@ -2,5 +2,5 @@ clear
 datename=$(date +%Y%m%d-%H%M%S)
 train_log=('genetic_iters_20_'$datename)
 mkdir log
-python3 search.py --max_iters=20 --batch_size=128 --workers=2 --net_cache='../training/models/checkpoint.pth.tar' | tee log/$train_log.search.log
+python3 -u search.py --max_iters=20 --batch_size=128 --workers=2 --net_cache='../training/models/checkpoint.pth.tar' | tee log/$train_log.search.log
 
